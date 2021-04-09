ce17f17668e2f73ab545295539c76f641eba68ae
shep4isa <82067783+shep4isa@users.noreply.github.com>
2021-04-08 21:05:38 -0500

    load files from treehouse

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+    quote: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+    source: "Michael Scott",
+    citation:"The Office",
+    year:""
+  },
+  {
+    quote:"Sometimes I'll start a sentence and I don't even know where it's going. I just hope I find it along the way.",
+    source: "Michael Scott",
+    citation:"The Office",
+    year:""
+  },
+  {
+    quote:"I'm not superstitious, but I am a little stitious.",
+    source: "Michael Scott",
+    citation:"The Office",
+    year:""
+  },
+  {
+    quote:"I love inside jokes. I hope to be a part of one someday.",
+    source: "Michael Scott",
+    citation:"The Office",
+    year:""
+  },
+  {
+    quote:"I feel like all my kids grew up and then they married each other. It’s every parent’s dream.",
+    source: "Michael Scott",
+    citation:"The Office",
+    year:""
+  },
+  {
+    quote:"And I knew exactly what to do. But in a much more real sense, I had no idea what to do.",
+    source: "Michael Scott",
+    citation:"The Office",
+    year:""
+  },
+  {
+    quote:"I… Declare…. Bankruptcy!",
+    source: "Michael Scott",
+    citation:"The Office",
+    year:""
+  }  
+]
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
